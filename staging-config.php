4d98efae40b85e7e3b6c9225de259bc12f87cffb
Matthew Sobieray <matt@hyfyn.com>
2016-06-06 17:04:49 -0600

    added staging-config.php

---
+++ staging-config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('DB_NAME', 'mtndog_localmilk');
+define('DB_USER', 'mtndog');
+define('DB_PASSWORD', 'Sobieray!2016.fyn');
+define('DB_HOST', 'localhost');
+$table_prefix  = 'lm_';
+define('WP_DEBUG', true);
+define( 'FORCE_SSL_ADMIN', false );
+define( 'SAVEQUERIES', false );
+define( 'JETPACK_DEV_DEBUG', true );
